4ce36a6e912cefd9fa194c11bd0c3d310013fc26
unknown <you@example.com>
2020-08-14 19:06:00 +0530

    Updates

---
+++ precache-manifest.972c98271e721dd55f76be477ec8af66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb47e7b2cc6e8b3ae87ca185a865d07",
+    "url": "/ReactAssignment/index.html"
+  },
+  {
+    "revision": "ae9c8af3c04fa237dd06",
+    "url": "/ReactAssignment/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9a80fe5a39a52b9db3dc",
+    "url": "/ReactAssignment/static/css/main.146e881f.chunk.css"
+  },
+  {
+    "revision": "ae9c8af3c04fa237dd06",
+    "url": "/ReactAssignment/static/js/2.b309af11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAssignment/static/js/2.b309af11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a80fe5a39a52b9db3dc",
+    "url": "/ReactAssignment/static/js/main.b8b447c5.chunk.js"
+  },
+  {
+    "revision": "5d87e79de8b4d1ac1295",
+    "url": "/ReactAssignment/static/js/runtime-main.81bf1aa9.js"
+  },
+  {
+    "revision": "60a4e7ef1e33d3ffeaff8f4e26658648",
+    "url": "/ReactAssignment/static/media/my-pic.60a4e7ef.jpg"
+  }
+]);
